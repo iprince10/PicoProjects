ebf2338428006c83a7d5e2c6df05e5cab2cae4bb
iprince10 <princejha1456@gmail.com>
2026-09-15 00:51:36 +0530

    new project file added

---
+++ Pico_SDCard_ILI9341/Pico_SDCard_ILI9341.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "ili9341.h"
+#include "timer.h"
+#include "uart.h"
+#include "bitmap.h"
+
+#define IO_BANK0_BASE 0x40014000u 
+#define GPIO_FUNC_SIO 5u
+#define GPIO25_CTRL (*(volatile uint32_t *)(IO_BANK0_BASE + 0xcc))
+
+#define SIO_BASE 0xd0000000u
+#define SIO_GPIO_OE (*(volatile uint32_t *)(SIO_BASE + 0x020))
+#define SIO_GPIO_OUT (*(volatile uint32_t *)(SIO_BASE + 0x010))
+#define GPIO25 (1u << 25)
+
+int main()
+{
+    GPIO25_CTRL = GPIO_FUNC_SIO;
+    SIO_GPIO_OE = GPIO25;
+    SIO_GPIO_OUT |= GPIO25;
+    delay_ms(500);
+    while (1)
+    {
+        SIO_GPIO_OUT &= ~GPIO25;
+        delay_ms(500);
+        SIO_GPIO_OUT |= GPIO25;
+        delay_ms(500);
+    }
+}
