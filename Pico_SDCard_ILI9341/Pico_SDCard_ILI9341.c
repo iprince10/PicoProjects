19e5b7b69832edf3b382e1caa8b909e9397ae23b
iprince10 <princejha1456@gmail.com>
2026-09-20 22:38:50 +0530

    project files updated

--- Pico_SDCard_ILI9341/Pico_SDCard_ILI9341.c
+++ Pico_SDCard_ILI9341/Pico_SDCard_ILI9341.c
@@ -6,8 +6,7 @@
 #include "bitmap.h"
 #include "sdcard.h"
 
-#define IO_BANK0_BASE 0x40014000u 
-
+#define IO_BANK0_BASE 0x40014000u
 
 #define GPIO_FUNC_SIO 5u
 #define GPIO25_CTRL (*(volatile uint32_t *)(IO_BANK0_BASE + 0xcc))
@@ -16,20 +15,32 @@
 #define SIO_GPIO_OUT (*(volatile uint32_t *)(SIO_BASE + 0x010))
 #define GPIO25 (1u << 25)
 
-
-int main()
+void led_init(void)
 {
     // blink code just for testing
     GPIO25_CTRL = GPIO_FUNC_SIO;
     SIO_GPIO_OE = GPIO25;
     SIO_GPIO_OUT |= GPIO25;
-    delay_ms(500);
+}
 
+int main()
+{
+    uart0_init();
+    spi1_init();
+    cs_init(); // after spi1 init always
+    sd_dummy_clocks();
+
+    uart0_puts("CMD0....\r\n");
+    if (sd_cmd0())
+    {
+        uart0_puts("CMD0 Ok - card in idle\r\n");
+    }
+    else
+    {
+        uart0_puts("CMD0 FAIL\r\n");
+    }
     while (1)
     {
-        SIO_GPIO_OUT &= ~GPIO25;
-        delay_ms(500);
-        SIO_GPIO_OUT |= GPIO25;
-        delay_ms(500);
+        delay_ms(1000);
     }
 }
